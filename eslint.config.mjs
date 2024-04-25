3ad24611c9fc9661c9f711cfb1a8a3867faa3c60
mnmustafa1109 <mnmustafa1109@gmail.com>
2024-04-25 13:10:22 +0500

    updated config for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
 ];
